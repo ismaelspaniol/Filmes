8bb567d25cf6509c903347ab50d20c982343492e
ismael <ismaelspaniol@outlook.com>
2019-03-22 15:00:31 -0300

    'teste'

--- gestao_clientes/urls_tenants.py
+++ gestao_clientes/urls_tenants.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from home import urls as home_urls
 from django.contrib.auth import views as auth_views
-from customers.views import TenantView
+
 
 urlpatterns = [
-    # path('', include(home_urls)),
-    path('', TenantView.as_view()),
+    path('', include(home_urls)),
     path('clientes/', include('clientes.urls',  namespace='clientes')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
